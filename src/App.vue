d4ee9dede6d53ec23d275275b71145b146867209
malone <43690259+malone081021@users.noreply.github.com>
2019-07-24 21:54:11 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MButton content="1" @on-click="handleClick"></MButton>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MButton from './components/MButtom'
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    MButton
+  },
+  methods:{
+    handleClick(event){
+      // eslint-disable-next-line no-console
+      console.log(event["name"] + event["age"] + " 我是父组件，我拿到事件了")
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MButtom.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <button  @click="handleClick">{{content}}</button>
+    </div>
+</template>
+
+<script>
+    // 判断参数是否是其中之一
+    function oneOf (value, validList) {
+        for (let i = 0; i < validList.length; i++) {
+            if (value === validList[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    export default {
+        name: "MButtom",
+        props:{
+            content: {
+                validator (value) {
+                    return oneOf(value, ['1', '2', '3']);
+                },
+                type:String,
+                default:"我是默认的内容"
+            }
+        },
+        methods:{
+            handleClick(event){
+                // eslint-disable-next-line no-console
+                console.log(event.toString())
+                var content = {name:"zilong",age:33};
+                this.$emit('on-click', content);
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
